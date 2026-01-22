2af603ab4f93e607b3ecd2f39a4433ba4ce46a2a
AliAhmedKhalil-cs <lyksab105@gmail.com>
2026-01-22 19:36:13 +0200

    Add files via upload

--- Creative Landing Page/main-landing.js
+++ Creative Landing Page/main-landing.js
@@ -1,5 +1,8 @@
-let img = document.querySelector('.img')
-let container = document.querySelector('.container')
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
 function phones(phone){
     img.src = phone;
 }
@@ -7,3 +10,33 @@ function phones(phone){
 function colors(color){
     container.style.background = color;
 }
+
+// Menu Toggle
+if(menuIcon){
+    menuIcon.onclick = () => {
+        navbar.classList.toggle('active');
+    }
+}
+
+// Close menu when clicking on a link
+if(navbar){
+    document.querySelectorAll('.navbar a').forEach(link => {
+        link.onclick = () => {
+            navbar.classList.remove('active');
+        }
+    })
+}
+
+// Smooth Scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if(target){
+            target.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
